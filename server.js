435c0096dbda6f3daea85e4049678d5a95f5516a
shiva prasad m <110959424+shivaprasadmakela@users.noreply.github.com>
2023-12-03 19:48:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5004;
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/contact';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const corsOptions = {
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  number: String,
+  message: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+app.post('/api/contact', async (req, res) => {
+  try {
+    const { name, email, number, message } = req.body;
+    const contact = new Contact({ name, email, number, message });
+    await contact.save();
+    res.status(201).json({ message: 'Contact saved successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  console.log('Received SIGTERM. Closing server gracefully.');
+  mongoose.connection.close(() => {
+    console.log('MongoDB connection closed.');
+    process.exit(0);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
